ed51696fa50306e011a18f9660d51e2492d70d29
tiecia <ty.cia@outlook.com>
2024-03-22 22:19:16 -0700

    81 current  2024-03-22 22:18:32  24.05.20240319.b06025f   6.6.22                          *

--- modules/nixos/configuration/rebuild-script.nix
+++ modules/nixos/configuration/rebuild-script.nix
@@ -9,7 +9,7 @@
       rebuild = "~/nix-configuration/nixos-rebuild.sh";
 
       sconf = "nano ~/nix-configuration/hosts/${CONFIGURATION_HOST}/configuration.nix";
-      hconf = "nano ~/nix-configuration/hosts/desktop/home.nix";
+      hconf = "nano ~/nix-configuration/hosts/desktop2/home.nix";
       nxrs = "sudo nixos-rebuild switch --flake ~/nix-configuration/#desktop";
       nxrt = "sudo nixos-rebuild test --flake ~/nix-configuration/#desktop";
     };
