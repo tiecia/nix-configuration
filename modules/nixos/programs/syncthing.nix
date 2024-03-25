a5ff181c3e365f16bbc245ef546d410444511c40
tiecia <ty.cia@outlook.com>
2024-03-24 22:17:46 -0700

    desktop 98 current  2024-03-24 21:23:09  24.05.20240323.44d0940   6.6.22                          *

--- modules/nixos/programs/syncthing.nix
+++ modules/nixos/programs/syncthing.nix
@@ -6,16 +6,23 @@
 }: let
   username = "tiec";
 in {
-  environment.systemPackages = with pkgs; [
-    syncthing
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   syncthing
+  # ];
 
   services = {
     syncthing = {
       enable = true;
-      user = "${username}";
-      dataDir = "/home/${username}/Documents"; # Default folder for new synced folders
-      configDir = "/home/${username}/.config/syncthing"; # Folder for Syncthing's settings and keys
+      # user = "${username}";
+      # dataDir = "/home/${username}/Documents"; # Default folder for new synced folders
+      # configDir = "/home/${username}/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
+      user = "tiec";
+      dataDir = "/home/tiec/Documents"; # Default folder for new synced folders
+      configDir = "/home/tiec/Documents/.config/syncthing"; # Folder for Syncthing's settings and keys
+      settings.gui = {
+        user = "ty";
+        password = "uda8BYJ^Qu5N&R";
+      };
     };
   };
 }
