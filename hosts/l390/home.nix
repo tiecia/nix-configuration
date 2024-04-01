6b9506bdbcfa34af6ca2f435c454a49581a78a7f
= <=>
2024-03-31 17:49:53 -0700

    l390 37 current  2024-03-29 22:57:54  24.05.20240327.2726f12   6.6.22                          *

--- hosts/l390/home.nix
+++ hosts/l390/home.nix
@@ -41,6 +41,13 @@
     # hello
   ];
 
+  programs.firefox = {
+    nativeMessagingHosts.packages = [
+      pkgs.firefoxpwa
+      # firefoxpwa-unwrapped
+    ];
+  };
+
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
