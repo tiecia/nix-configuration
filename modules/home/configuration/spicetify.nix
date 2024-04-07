4f1f6eded21e45c7179e020622d3ba4f5138f345
tiecia <ty.cia@outlook.com>
2024-04-07 00:02:44 -0700

    desktop 148 current  2024-04-07 00:02:37  24.05.20240327.2726f12   6.6.22                          *

--- modules/home/configuration/spicetify.nix
+++ modules/home/configuration/spicetify.nix
@@ -25,22 +25,45 @@ in {
 
     customColorScheme = {
       text = "ffffff";
-      # subtext = "F0F0F0";
-      # sidebar-text = "e0def4";
-      # main = "191724";
-      # sidebar = "2a2837";
-      # player = "191724";
-      # card = "191724";
-      # shadow = "1f1d2e";
-      # selected-row = "797979";
-      button = "ff0000";
-      button-active = "00ff00";
-      # button-disabled = "555169";
-      # tab-active = "ebbcba";
-      # notification = "1db954";
-      # notification-error = "eb6f92";
-      # misc = "6e6a86";
+      subtext = "ffffff";
+      nav-active-text = "ffffff";
+      main = "020816";
+      sidebar = "051024";
+      player = "030b1e";
+      card = "0a1527";
+      shadow = "000000";
+      main-secondary = "06142d";
+      button = "1464b5";
+      button-secondary = "ffffff";
+      button-active = "4a99e9";
+      button-disabled = "21282f";
+      nav-active = "37b778";
+      play-button = "37b778";
+      tab-active = "0a1527";
+      notification = "051024";
+      notification-error = "051024";
+      playback-bar = "37b778";
+      misc = "FFFFFF";
     };
+
+    # customColorScheme = {
+    #   text = "ffffff";
+    #   # subtext = "F0F0F0";
+    #   # sidebar-text = "e0def4";
+    #   # main = "191724";
+    #   # sidebar = "2a2837";
+    #   # player = "191724";
+    #   # card = "191724";
+    #   # shadow = "1f1d2e";
+    #   # selected-row = "797979";
+    #   button = "ff0000";
+    #   button-active = "00ff00";
+    #   # button-disabled = "555169";
+    #   # tab-active = "ebbcba";
+    #   # notification = "1db954";
+    #   # notification-error = "eb6f92";
+    #   # misc = "6e6a86";
+    # };
 
     enabledExtensions = with spicePkgs.extensions; [
       shuffle # shuffle+ (special characters are sanitized out of ext names)
