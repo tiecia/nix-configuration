87ecb98ed0903c3ae739579f8ee8a41f50ca960e
tiecia <ty.cia@outlook.com>
2024-04-06 23:40:34 -0700

    desktop 137 current  2024-04-06 23:40:28  24.05.20240327.2726f12   6.6.22                          *

--- modules/home/configuration/spicetify.nix
+++ modules/home/configuration/spicetify.nix
@@ -19,7 +19,7 @@ in {
     enable = true;
 
     theme = spicePkgs.themes.Sleek;
-    colorScheme = "deeper";
+    colorScheme = "deep";
 
     enabledExtensions = with spicePkgs.extensions; [
       shuffle # shuffle+ (special characters are sanitized out of ext names)
