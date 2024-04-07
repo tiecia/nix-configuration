c2341b59b09f8c85cf0c77ab39d079311e48a054
tiecia <ty.cia@outlook.com>
2024-04-06 23:49:45 -0700

    desktop 141 current  2024-04-06 23:49:38  24.05.20240327.2726f12   6.6.22                          *

--- modules/home/configuration/spicetify.nix
+++ modules/home/configuration/spicetify.nix
@@ -24,22 +24,22 @@ in {
     colorScheme = "custom";
 
     customColorScheme = {
-      text = "ebbcba";
-      subtext = "F0F0F0";
-      sidebar-text = "e0def4";
-      main = "191724";
-      sidebar = "2a2837";
-      player = "191724";
-      card = "191724";
-      shadow = "1f1d2e";
-      selected-row = "797979";
+      # text = "ebbcba";
+      # subtext = "F0F0F0";
+      # sidebar-text = "e0def4";
+      # main = "191724";
+      # sidebar = "2a2837";
+      # player = "191724";
+      # card = "191724";
+      # shadow = "1f1d2e";
+      # selected-row = "797979";
       button = "31748f";
-      button-active = "31748f";
-      button-disabled = "555169";
-      tab-active = "ebbcba";
-      notification = "1db954";
-      notification-error = "eb6f92";
-      misc = "6e6a86";
+      # button-active = "31748f";
+      # button-disabled = "555169";
+      # tab-active = "ebbcba";
+      # notification = "1db954";
+      # notification-error = "eb6f92";
+      # misc = "6e6a86";
     };
 
     enabledExtensions = with spicePkgs.extensions; [
