1ced471c270f4a273a9d61d301b686d5611027f8
tiecia <ty.cia@outlook.com>
2024-04-06 23:50:30 -0700

    desktop 142 current  2024-04-06 23:50:23  24.05.20240327.2726f12   6.6.22                          *

--- modules/home/configuration/spicetify.nix
+++ modules/home/configuration/spicetify.nix
@@ -24,7 +24,7 @@ in {
     colorScheme = "custom";
 
     customColorScheme = {
-      # text = "ebbcba";
+      text = "ebbcba";
       # subtext = "F0F0F0";
       # sidebar-text = "e0def4";
       # main = "191724";
