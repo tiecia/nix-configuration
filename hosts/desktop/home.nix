5ac579b1b2983af2e7d4fe70c8f8cd48f7e65c0d
tiecia <ty.cia@outlook.com>
2024-04-08 22:26:20 -0700

    desktop 160 current  2024-04-08 22:26:15  24.05.20240327.2726f12   6.6.22                          *

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -36,7 +36,38 @@
 
   spotify = {
     # theme = builtins.trace "Spicetify Theme: ${builtins.toJSON spotheme}" spotheme;
-    # theme = {};
+    theme = {
+      enable = true;
+
+      # https://github.com/spicetify/spicetify-themes/tree/master/Sleek
+      theme = spicetify-nix.packages.${pkgs.system}.default.themes.Sleek;
+      # colorScheme = "deep";
+
+      colorScheme = "custom";
+
+      customColorScheme = {
+        text = "ffffff";
+        subtext = "ffffff";
+        nav-active-text = "ffffff";
+        main = "020816";
+        sidebar = "051024";
+        player = "030b1e";
+        card = "0a1527";
+        shadow = "000000";
+        main-secondary = "06142d";
+        button = "1DB954";
+        button-secondary = "ffffff";
+        button-active = "1DB954";
+        button-disabled = "21282f";
+        nav-active = "37b778";
+        play-button = "37b778";
+        tab-active = "1DB954";
+        notification = "051024";
+        notification-error = "051024";
+        playback-bar = "37b778";
+        misc = "FFFFFF";
+      };
+    };
   };
 
   # This value determines the Home Manager release that your configuration is
