10a6fd18e53fe467c3046332edbf3df71952fa71
= <=>
2024-04-09 16:23:05 -0700

    l390 46 current  2024-04-09 16:22:59  24.05.20240327.2726f12   6.6.22                          *

--- hosts/l390/home.nix
+++ hosts/l390/home.nix
@@ -32,7 +32,34 @@
 
   spotify = {
     enable = true;
-    # theme = {};
+    theme = {
+      # https://github.com/spicetify/spicetify-themes/tree/master/Sleek
+      theme = spicetify-nix.packages.${pkgs.system}.default.themes.Sleek;
+      # colorScheme = "deep";
+      colorScheme = "custom";
+      customColorScheme = {
+        text = "ffffff";
+        subtext = "ffffff";
+        nav-active-text = "ffffff";
+        main = "020816";
+        sidebar = "051024";
+        player = "030b1e";
+        card = "0a1527";
+        shadow = "000000";
+        main-secondary = "06142d";
+        button = "1DB954";
+        button-secondary = "ffffff";
+        button-active = "1DB954";
+        button-disabled = "21282f";
+        nav-active = "37b778";
+        play-button = "37b778";
+        tab-active = "1DB954";
+        notification = "051024";
+        notification-error = "051024";
+        playback-bar = "37b778";
+        misc = "FFFFFF";
+      };
+    };
   };
 
   # Home Manager needs a bit of information about you and the paths it should
