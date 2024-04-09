e24f96f06214a70fa53d742c6dee8de0df15eae0
= <=>
2024-04-09 16:16:00 -0700

    l390 43 current  2024-04-09 16:15:55  24.05.20240327.2726f12   6.6.22                          *

--- hosts/l390/home.nix
+++ hosts/l390/home.nix
@@ -30,6 +30,10 @@
   #   theme = {};
   # };
 
+  spotify = {
+    enable = true;
+  };
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "tiec";
