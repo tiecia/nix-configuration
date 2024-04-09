934aa4e8f5295d361fb922739914f9a9883939c8
= <=>
2024-04-09 12:26:57 -0700

    l390 39 current  2024-04-09 12:26:50  24.05.20240327.2726f12   6.6.22                          *

--- hosts/l390/home.nix
+++ hosts/l390/home.nix
@@ -16,7 +16,8 @@
     ../../modules/home/programs/msteams.nix
     ../../modules/home/programs/onedrive.nix
     ../../modules/home/programs/solaar.nix
-    ../../modules/home/programs/spotify-old.nix
+    ../../modules/home/programs/spotify.nix
+    # ../../modules/home/programs/spotify-old.nix
     ../../modules/home/programs/wireguard.nix
     ../../modules/home/programs/filezilla.nix
     ../../modules/home/programs/libreoffice.nix
@@ -24,6 +25,10 @@
 
     spicetify-nix.homeManagerModule
   ];
+
+  spotify = {
+    theme = {};
+  };
 
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
