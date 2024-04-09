6c517c1a77ffca66c9b44d12b17bd15409514381
tiecia <ty.cia@outlook.com>
2024-04-08 18:08:55 -0700

    desktop 157 current  2024-04-08 11:02:13  24.05.20240327.2726f12   6.6.22                          *

--- modules/home/programs/spotify.nix
+++ modules/home/programs/spotify.nix
@@ -4,27 +4,27 @@
   pkgs,
   spicetify-nix,
   ...
-}:
-with lib; {
-  options = {
-    spotify.theme = mkOption {
-      type = types.anything;
-      default = null;
-      description = "Use Spicetify theme";
+}: let
+  x = "test";
+in
+  with lib; {
+    options = {
+      spotify.theme = mkOption {
+        type = types.anything;
+        default = null;
+        description = "Use Spicetify theme";
+      };
     };
-  };
 
-  config = {
-    home.packages = mkIf (config.spotify.theme == null) [pkgs.spotify];
+    config = {
+      home.packages = mkIf (config.spotify.theme == null) [pkgs.spotify];
 
-    # imports = mkIf (config.spotify.theme != null) [spicetify-nix.homeManagerModule];
+      # imports = mkIf (config.spotify.theme != null) [spicetify-nix.homeManagerModule];
 
-    # _ = builtins.trace "Theme: ${builtins.toJSON config.spotify.theme}" config.spotify.theme;
-
-    programs.spicetify =
-      mkIf (config.spotify.theme != null) {
-        enable = true;
-      }
-      // builtins.trace "Theme: ${builtins.toJSON config.spotify.theme}" config.spotify.theme;
-  };
-}
+      programs.spicetify =
+        mkIf (builtins.trace "Theme: ${builtins.toJSON config.spotify.theme}" config.spotify.theme != null) {
+          enable = true;
+        }
+        // config.spotify.theme;
+    };
+  }
