1066e831d33da6589edfbcbef387a8363867f1b7
tiecia <ty.cia@outlook.com>
2024-04-10 17:36:44 -0700

    desktop 186 current  2024-04-10 17:36:35  24.05.20240410.c463436   6.6.26                          *

--- modules/home/programs/desktop/firefox.nix
+++ modules/home/programs/desktop/firefox.nix
@@ -7,11 +7,28 @@
 with lib; {
   options = {
     firefox.enable = lib.mkEnableOption "Enable firefox";
+    firefox.installPWA = lib.mkEnableOption "Enable PWA";
   };
 
   config = mkIf config.firefox.enable {
-    home.packages = with pkgs; [
-      firefox
-    ];
+    home.packages = with pkgs;
+      mkMerge [
+        [firefox]
+        (mkIf config.firefox.installPWA [firefoxpwa])
+      ];
+
+    programs.firefox = {
+      enable = true;
+      nativeMessagingHosts = [pkgs.firefoxpwa];
+      # nativeMessagingHosts.packages = mkIf config.firefox.installPWA [pkgs.firefoxpwa];
+    };
   };
 }
+# environment.systemPackages = with pkgs; [
+#   firefoxpwa
+# ];
+# programs.firefox = {
+#   enable = true;
+#   nativeMessagingHosts.packages = [pkgs.firefoxpwa];
+# };
+
