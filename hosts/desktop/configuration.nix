9fc9ff92770821e78d0b7a14115e7a46ced3675e
tiecia <ty.cia@outlook.com>
2024-04-12 15:55:46 -0700

    desktop 192 current  2024-04-12 15:55:38  24.05.20240410.c463436   6.6.26                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -34,6 +34,7 @@ in {
     ../../modules/nixos/configuration/bootloader.nix
     ../../modules/nixos/configuration/rebuild.nix
     ../../modules/nixos/configuration/numlock.nix
+    ../../modules/nixos/configuration/flatpack.nix
 
     # Desktop environment
     ../../modules/nixos/desktop-environment/kde-plasma.nix
