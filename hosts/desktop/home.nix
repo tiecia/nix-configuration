12d33bf7c391ee64e0fc16b7fe5ed47b610eec1b
tiecia <ty.cia@outlook.com>
2024-04-12 23:28:16 -0700

    desktop 195 current  2024-04-12 23:28:09  24.05.20240410.1042fd8   6.6.26                          *

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -52,6 +52,12 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    "~/.config/onedrive".text = ''
+      !/Documents/2008 Nordhavn 64 PERSEVERANCE
+      !/Documents/Backups
+
+      /Documents
+    '';
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
