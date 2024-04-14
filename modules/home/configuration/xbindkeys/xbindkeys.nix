ff755dd7890de42535ca596b12eb8d0b0103bfa2
tiecia <ty.cia@outlook.com>
2024-04-13 23:27:14 -0700

    keys

---
+++ modules/home/configuration/xbindkeys/xbindkeys.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib; {
+  options = {
+    xbindkeys.enable = mkEnableOption "Enable xbindkeys";
+
+    # xbindkeys.mxmaster-playpause.enable = mkEnableOption "Remap forward button to play/pause on MX Master";
+  };
+
+  config = mkIf config.xbindkeys.enable {
+    home.file.".xbindkeysrc".source = ./.xbindkeysrc;
+
+    home.packages = with pkgs; [
+      xbindkeys
+    ];
+  };
+}
