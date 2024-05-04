598da065ef194e68e9cb5d686ce932d7fdea5e26
tiecia <ty.cia@outlook.com>
2024-05-03 20:08:25 -0700

    desktop 265 current  2024-05-03 20:08:24  24.05.20240502.63c3a29  6.6.29                          *

--- modules/nixos/programs/steam.nix
+++ modules/nixos/programs/steam.nix
@@ -16,6 +16,7 @@ with lib; {
 
     programs.steam = {
       enable = true;
+      gamescopeSession.enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
       dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     };
