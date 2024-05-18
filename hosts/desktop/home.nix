9e25baced4c7b355d7e2a7f327a63c8c49aab85e
tiecia <ty.cia@outlook.com>
2024-05-17 21:57:01 -0700

    desktop 279 current  2024-05-17 19:13:07  24.05.20240515.33d1e75  6.6.30                          *

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -38,6 +38,11 @@
 
   xbindkeys.enable = true;
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = pkgs.hyprland;
+  };
+
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "tiec";
