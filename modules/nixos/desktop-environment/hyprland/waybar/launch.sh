cf746ca4ee4ad48694051cda6e410b418bf00d20
tiecia <ty.cia@outlook.com>
2024-05-19 20:39:23 -0700

    working

--- modules/nixos/desktop-environment/hyprland/waybar/launch.sh
+++ modules/nixos/desktop-environment/hyprland/waybar/launch.sh
@@ -1,10 +1,21 @@
-i#!/bin/bash
+#!/bin/bash
 #  ____  _             _    __        __          _                 
 # / ___|| |_ __ _ _ __| |_  \ \      / /_ _ _   _| |__   __ _ _ __  
 # \___ \| __/ _` | '__| __|  \ \ /\ / / _` | | | | '_ \ / _` | '__| 
 #  ___) | || (_| | |  | |_    \ V  V / (_| | |_| | |_) | (_| | |    
 # |____/ \__\__,_|_|   \__|    \_/\_/ \__,_|\__, |_.__/ \__,_|_|    
 #                                           |___/                   
+# by Stephan Raabe (2023) 
+# ----------------------------------------------------- 
+
+waybar_config_root="$HOME/nix-configuration/modules/nixos/desktop-environment/hyprland/waybar"
+
+# Check if waybar-disabled file exists
+if [ -f $HOME/.cache/waybar-disabled ] ;then 
+    killall waybar
+    pkill waybar
+    exit 1 
+fi
 
 # ----------------------------------------------------- 
 # Quit all running waybar instances
@@ -31,7 +42,7 @@ fi
 IFS=';' read -ra arrThemes <<< "$themestyle"
 echo "Theme: ${arrThemes[0]}"
 
-if [ ! -f ~/dotfiles/waybar/themes${arrThemes[1]}/style.css ]; then
+if [ ! -f $waybar_config_root/themes${arrThemes[1]}/style.css ]; then
     themestyle="/ml4w;/ml4w/light"
 fi
 
@@ -42,12 +53,17 @@ config_file="config"
 style_file="style.css"
 
 # Standard files can be overwritten with an existing config-custom or style-custom.css
-if [ -f ~/dotfiles/waybar/themes${arrThemes[0]}/config-custom ] ;then
+if [ -f $waybar_config_root/themes${arrThemes[0]}/config-custom ] ;then
     config_file="config-custom"
 fi
-if [ -f ~/dotfiles/waybar/themes${arrThemes[1]}/style-custom.css ] ;then
+if [ -f $waybar_config_root/themes${arrThemes[1]}/style-custom.css ] ;then
     style_file="style-custom.css"
 fi
 
-waybar -c ~/dotfiles/waybar/themes${arrThemes[0]}/$config_file -s ~/dotfiles/waybar/themes${arrThemes[1]}/$style_file &
+echo "Style Used:"
+echo $waybar_config_root/themes${arrThemes[1]}/$style_file
 
+echo "Config Used"
+echo $waybar_config_root/themes${arrThemes[0]}/$config_file
+
+waybar -c $waybar_config_root/themes${arrThemes[0]}/$config_file -s $waybar_config_root/themes${arrThemes[1]}/$style_file &
