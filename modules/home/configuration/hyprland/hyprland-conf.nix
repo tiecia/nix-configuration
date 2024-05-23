7d5a3afe8db213f13e556cfb9ef53d1d29390076
tiecia <ty.cia@outlook.com>
2024-05-22 17:14:34 -0700

    desktop 306 current  2024-05-22 17:14:32  24.05.20240519.6c0b7a9  6.6.31                          hyprland

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -36,11 +36,61 @@ in
           ];
 
           input = {
-            kb_variant = "us";
+            kb_layout = "us";
+            kb_variant = "";
+            kb_model = "";
+            kb_options = "ctrl:nocaps";
+            kb_rules = "";
+            follow_mouse = "1";
+            touchpad = {
+              natural_scroll = "no";
+            };
+
+            sensitivity = "-0.5";
+          };
+
+          general = {
+            gaps_in = "2";
+            gaps_out = "0";
+            border_size = "0";
+            "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
+            "col.inactive_border" = "rgba(595959aa)";
+
+            layout = "dwindle";
+
+            allow_tearing = "false";
           };
 
           "$mainMod" = "SUPER";
           bind = "$mainMod, F, exec, firefox";
+
+          decoration = {
+            rounding = "10";
+            blur = {
+              enabled = "true";
+              size = "3";
+              passes = "1";
+            };
+            drop_shadow = "yes";
+            shadow_range = "4";
+            shadow_render_power = "3";
+            "col.shadow" = "rgba(1a1a1aee)";
+          };
+
+          animation = {
+            enabled = "yes";
+
+            bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
+
+            animation = [
+              "windows, 1, 7, myBezier"
+              "windowsOut, 1, 7, default, popin 80%"
+              "border, 1, 10, default"
+              "borderangle, 1, 8, default"
+              "fade, 1, 7, default"
+              "workspaces, 1, 6, default"
+            ];
+          };
         };
       };
 
