70810a02563d2b76e70f8265fec548d66d2270c6
tiecia <ty.cia@outlook.com>
2024-05-22 17:35:26 -0700

    desktop 309 current  2024-05-22 17:35:23  24.05.20240519.6c0b7a9  6.6.31                          hyprland

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -17,8 +17,19 @@ in
       wayland.windowManager.hyprland = {
         enable = true;
         settings = {
+          monitor = [
+            "DP-1,2560x1080@144,0x0,1 # Main Ultrawide"
+            "HDMI-A-1,2560x1080@60,0x-1080,1" #Top Ultrawide
+            "HDMI-A-2,1920x1080@60,-1080x-840,1,transform,1" #Left Vertical
+            "DP-2,1920x1080@60,2560x-700,1,transform,1" #Right Vertical
+          ];
+
+          # See https://wiki.hyprland.org/Configuring/Keywords/ for more
+          # For all categories, see https://wiki.hyprland.org/Configuring/Variables/
+
           exec-once = ''${startupScript}/bin/start'';
 
+          # See https://wiki.hyprland.org/Configuring/Keywords/ for more
           "$config-root" = "~/nix-configuration/modules/nixos/desktop-environment/hyprland";
           "$rofi-script" = "bash $config_root/../rofi.sh";
 
@@ -30,7 +41,63 @@ in
           "$screenshot-region" = "hyprshot -m region --clipboard-only";
 
           "$mainMod" = "SUPER";
-          bind = "$mainMod, F, exec, firefox";
+          bind = [
+            # See https://wiki.hyprland.org/Configuring/Binds/ for more
+            "$mainMod, F, exec, firefox"
+            "$mainMod, C, killactive,"
+            "$mainMod, M, exit,"
+            "$mainMod, E, exec, $fileManager"
+            "$mainMod, V, togglefloating,"
+            "$mainMod, SUPER_L, exec, $menu"
+            "$mainMod, R, exec, $menu-all"
+            "$mainMod, P, pseudo," # dwindle
+            "$mainMod, J, togglesplit," # dwindle
+
+            "$mainMod, B, exec, $waybar-switch"
+            "$mainMod&Shift_L, S, exec, $screenshot-region"
+
+            # Move focus with mainMod + arrow keys
+            "$mainMod, left, movefocus, l"
+            "$mainMod, right, movefocus, r"
+            "$mainMod, up, movefocus, u"
+            "$mainMod, down, movefocus, d"
+
+            # Switch workspaces with mainMod + [0-9]
+            "$mainMod, 1, workspace, 1"
+            "$mainMod, 2, workspace, 2"
+            "$mainMod, 3, workspace, 3"
+            "$mainMod, 4, workspace, 4"
+            "$mainMod, 5, workspace, 5"
+            "$mainMod, 6, workspace, 6"
+            "$mainMod, 7, workspace, 7"
+            "$mainMod, 8, workspace, 8"
+            "$mainMod, 9, workspace, 9"
+            "$mainMod, 0, workspace, 10"
+
+            # Move active window to a workspace with mainMod + SHIFT + [0-9]
+            "$mainMod SHIFT, 1, movetoworkspace, 1"
+            "$mainMod SHIFT, 2, movetoworkspace, 2"
+            "$mainMod SHIFT, 3, movetoworkspace, 3"
+            "$mainMod SHIFT, 4, movetoworkspace, 4"
+            "$mainMod SHIFT, 5, movetoworkspace, 5"
+            "$mainMod SHIFT, 6, movetoworkspace, 6"
+            "$mainMod SHIFT, 7, movetoworkspace, 7"
+            "$mainMod SHIFT, 8, movetoworkspace, 8"
+            "$mainMod SHIFT, 9, movetoworkspace, 9"
+            "$mainMod SHIFT, 0, movetoworkspace, 10"
+
+            # Example special workspace (scratchpad)
+            "$mainMod, S, togglespecialworkspace, magic"
+            # bind = $mainMod SHIFT, S, movetoworkspace, special:magic
+
+            # Scroll through existing workspaces with mainMod + scroll
+            "$mainMod, mouse_down, workspace, e+1"
+            "$mainMod, mouse_up, workspace, e-1"
+
+            # Move/resize windows with mainMod + LMB/RMB and dragging
+            "$mainMod, mouse:272, movewindow:"
+            "$mainMod, mouse:273, resizewindow"
+          ];
 
           # Some default env vars.
           env = [
@@ -61,6 +128,7 @@ in
 
             layout = "dwindle";
 
+            # Please see https://wiki.hyprland.org/Configuring/Tearing/ before you turn this on
             allow_tearing = "false";
           };
 
@@ -78,6 +146,8 @@ in
           };
 
           animations = {
+            # Some default animations, see https://wiki.hyprland.org/Configuring/Animations/ for more
+
             enabled = "yes";
 
             bezier = "myBezier, 0.05, 0.9, 0.1, 1.05";
@@ -93,27 +163,38 @@ in
           };
 
           dwindle = {
+            # See https://wiki.hyprland.org/Configuring/Dwindle-Layout/ for more
             pseudotile = "yes";
             preserve_split = "yes";
           };
 
           master = {
+            # See https://wiki.hyprland.org/Configuring/Master-Layout/ for more
             new_is_master = "true";
           };
 
           gestures = {
+            # See https://wiki.hyprland.org/Configuring/Variables/ for more
             workspace_swipe = "off";
           };
 
           misc = {
+            # See https://wiki.hyprland.org/Configuring/Variables/ for more
             force_default_wallpaper = "1";
           };
 
+          # Example per-input-device config
+          # See https://wiki.hyprland.org/Configuring/Keywords/#per-device-input-configs for more
           device = {
             name = "epic-mouse-v1";
             sensitivity = "-0.5";
           };
 
+          # Example windowrule v1
+          # windowrule = float, ^(kitty)$
+          # Example windowrule v2
+          # windowrulev2 = float,class:^(kitty)$,title:^(kitty)$
+          # See https://wiki.hyprland.org/Configuring/Window-Rules/ for more
           windowrulev2 = "suppressevent maximize, class:.*";
         };
       };
