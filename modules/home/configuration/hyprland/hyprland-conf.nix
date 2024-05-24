25b18cea8767a425444c573680974a3f19c0bcfc
tiecia <ty.cia@outlook.com>
2024-05-23 21:25:48 -0700

    desktop 335 current  2024-05-23 21:25:45  24.05.20240519.6c0b7a9  6.6.31                          hyprland

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -108,7 +108,7 @@ in
             "$mainMod Ctrl_L, down, movewindow, d"
 
             # Move to desktop system specific config
-            "$mainMod, P, swapactiveworkspaces, 0 1"
+            "$mainMod, P, swapactiveworkspaces, 0 2"
           ];
 
           bindm = [
