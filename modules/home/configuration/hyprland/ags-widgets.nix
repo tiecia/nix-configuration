70cd2fadbd01dafa124511233c74d7a269613699
tiecia <ty.cia@outlook.com>
2024-06-10 17:40:00 -0700

    desktop 445 current  2024-06-10 17:39:58  24.11.20240529.ad57eef  6.6.32                          hyprland plasma

--- modules/home/configuration/hyprland/ags-widgets.nix
+++ modules/home/configuration/hyprland/ags-widgets.nix
@@ -16,14 +16,18 @@
   config = let
     options = config.widgets.ags;
     startupScript = pkgs.pkgs.writeShellScriptBin "startupScript" ''
-      #${pkgs.ags}/bin/ags -b hypr
+      #${pkgs.ags}/bin/ags -b hypr # Why does calling pkgs.ags not work?
       ags -b hypr
     '';
+    mainMod = "Super"; # Inherit this from parent config?
   in
     lib.mkIf options.enable {
       wayland.windowManager.hyprland = {
         settings = {
           exec-once = ''${startupScript}/bin/startupScript'';
+          bind = [
+            "${mainMod},${mainMod}_L, exec, ags -t launcher"
+          ];
         };
       };
 
