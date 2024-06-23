d174c995a7a78831e76e3ece46409e163cac27fe
tiecia <ty.cia@outlook.com>
2024-06-22 19:08:11 -0700

    sls 72 current  2024-06-22 19:08:09  24.11.20240620.d603719  6.9.3                           plasma

--- hosts/sls/home.nix
+++ hosts/sls/home.nix
@@ -47,7 +47,7 @@
     kitty = {
       enable = true; # Kitty is installed by Hyprland so we need to enable it here for options.
       font = {
-        size = 30;
+        size = 20;
         name = "DejaVu Sans";
       };
     };
