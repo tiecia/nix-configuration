0832ae76667598aaa957d949361d18782d6c436d
tiecia <ty.cia@outlook.com>
2024-06-24 19:22:38 -0700

    sls 119 current  2024-06-24 19:22:37  24.11.20240620.d603719  6.9.3                           plasma

--- hosts/sls/configuration.nix
+++ hosts/sls/configuration.nix
@@ -35,10 +35,11 @@ in {
   printing.enable = true;
 
   nvidia-graphics = {
-    enable = false;
+    enable = true;
     prime = "offload";
     intelBusId = "PCI:0:2:0";
-    nvidiaBusId = "PCI:243:0:0";
+    nvidiaBusId = "PCI:242:0:0";
+    # nvidiaBusId = "PCI:243:0:0";
   };
 
   microsoft-surface.ipts.enable = true;
