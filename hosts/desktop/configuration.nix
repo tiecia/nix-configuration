299978aeed11bae4aa5fde6926cb4d80f1da6cf7
tiecia <ty.cia@outlook.com>
2024-06-26 20:35:22 -0700

    desktop 503 current  2024-06-26 20:35:20  24.11.20240624.2893f56  6.6.35                          plasma

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -38,6 +38,12 @@ in {
     # };
   };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    # Add any missing dynamic libraries for unpackaged
+    # programs here, NOT in environment.systemPackages
+  ];
+
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [5000 1883];
