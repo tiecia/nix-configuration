d1fa76a08bd497b5be4bbadb8f6c98eb2f2396e5
tiecia <ty.cia@outlook.com>
2024-06-28 21:11:13 -0700

    sls 142 current  2024-06-28 21:11:11  24.11.20240624.2893f56  6.9.3                           plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -154,6 +154,9 @@ in
               "XCURSOR_SIZE,24"
               "QT_QPA_PLATFORMTHEME,qt5ct" # change to qt6ct if you have that
               "ELECTRON_OZONE_PLATFORM_HINT,auto"
+
+              "GTK_USE_PORTAL,0"
+
               # "WLR_NO_HARDWARE_CURSORS,1" # This has been deprecated in favor of the cursor variable.
               "NIXOS_OZONE_WL,1"
 
