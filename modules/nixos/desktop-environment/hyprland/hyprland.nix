ea16106dfe8b923e517359757bc4ac07200eb7ca
tiecia <ty.cia@outlook.com>
2024-06-28 21:03:32 -0700

    sls 141 current  2024-06-28 21:03:30  24.11.20240624.2893f56  6.9.3                           plasma

--- modules/nixos/desktop-environment/hyprland/hyprland.nix
+++ modules/nixos/desktop-environment/hyprland/hyprland.nix
@@ -22,13 +22,24 @@ with lib; {
       xserver = {
         enable = true;
         libinput.enable = true;
-        displayManager.gdm = {
-          enable = true;
-          wayland = true;
-        };
+        # displayManager.gdm = {
+        #   enable = true;
+        #   wayland = true;
+        # };
       };
 
       gnome.gnome-keyring.enable = true;
+    };
+
+    services.greetd = {
+      enable = true;
+      settings = rec {
+        initial_session = {
+          command = "${pkgs.sway}/bin/sway";
+          user = "myuser";
+        };
+        default_session = initial_session;
+      };
     };
 
     security = {
