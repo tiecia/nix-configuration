7b55bea3d2e128d003f184cdad7faea342ad6c55
tiecia <ty.cia@outlook.com>
2024-07-02 18:57:18 -0700

    sls 198 current  2024-07-02 18:57:15  24.11.20240624.2893f56  6.9.3                           gnome plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -311,9 +311,40 @@ in
           };
           mimeApps = {
             enable = true;
+            associations.added = {
+              "x-scheme-handler/notion" = ["notion-app-enhanced.desktop"];
+              "x-scheme-handler/gitkraken" = ["GitKraken.desktop"];
+              "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+              "x-scheme-handler/http" = ["firefox.desktop"];
+              "x-scheme-handler/https" = ["firefox.desktop"];
+              "x-scheme-handler/chrome" = ["firefox.desktop"];
+              "text/html" = ["firefox.desktop"];
+              "application/x-extension-htm" = ["firefox.desktop"];
+              "application/x-extension-html" = ["firefox.desktop"];
+              "application/x-extension-shtml" = ["firefox.desktop"];
+              "application/xhtml+xml" = ["firefox.desktop"];
+              "application/x-extension-xhtml" = ["firefox.desktop"];
+              "application/x-extension-xht" = ["firefox.desktop"];
+            };
+
             defaultApplications = {
+              "x-scheme-handler/notion" = ["notion-app-enhanced.desktop"];
+              "x-scheme-handler/gitkraken" = ["GitKraken.desktop"];
+              "x-scheme-handler/msteams" = ["teams-for-linux.desktop"];
+              "text/html" = ["firefox.desktop"];
+              "x-scheme-handler/http" = ["firefox.desktop"];
+              "x-scheme-handler/https" = ["firefox.desktop"];
+              "x-scheme-handler/about" = ["google-chrome.desktop"];
+              "x-scheme-handler/unknown" = ["google-chrome.desktop"];
+              "x-scheme-handler/chrome" = ["firefox.desktop"];
+              "application/x-extension-htm" = ["firefox.desktop"];
+              "application/x-extension-html" = ["firefox.desktop"];
+              "application/x-extension-shtml" = ["firefox.desktop"];
+              "application/xhtml+xml" = ["firefox.desktop"];
+              "application/x-extension-xhtml" = ["firefox.desktop"];
+              "application/x-extension-xht" = ["firefox.desktop"];
+
               "application/pdf" = ["org.pwmt.zathura.desktop"];
-              # "image/*" = ["com.interversehq.qView.desktop"];
             };
           };
         };
