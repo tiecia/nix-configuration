a6360fa9dec8e5eaec84094e53cf8e399bec3dfe
tiecia <ty.cia@outlook.com>
2024-07-17 11:14:05 -0700

    sls 245 current  2024-07-17 11:14:03  24.11.20240709.feb2849  6.9.3                           gnome plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -281,7 +281,7 @@ in
                 "border, 1, 10, default"
                 "borderangle, 1, 8, default"
                 "fade, 1, 7, default"
-                "workspaces, 0, 1, default"
+                "workspaces, 1, 1, default"
               ];
             };
 
