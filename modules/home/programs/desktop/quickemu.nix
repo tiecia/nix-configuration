22dfe74dca651f4cc66cc8ba0abd643ccda8a655
tiecia <ty.cia@outlook.com>
2024-07-19 13:02:13 -0700

    sls 271 current  2024-07-19 13:02:12  24.11.20240716.ad0b5ee  6.9.3                           gnome plasma

--- modules/home/programs/desktop/quickemu.nix
+++ modules/home/programs/desktop/quickemu.nix
@@ -3,15 +3,33 @@
   lib,
   pkgs,
   ...
-}:
-with lib; {
-  options = {
-    quickemu.enable = lib.mkEnableOption "Enable quickemu";
-  };
+}: let
+  configDir = config.home.homeDirectory + "/.quickemu";
+  windows11-setup = pkgs.writeShellScriptBin "windows11-setup" ''
+    cd ${configDir}
+    quickget windows 11
+    quickemu --vm windows-11.conf --shortcut
+  '';
+  windows11 = pkgs.writeShellScriptBin "windows11" ''
+    quickemu --vm ${configDir}/windows-11.conf
+  '';
+in
+  with lib; {
+    options = {
+      quickemu.enable = lib.mkEnableOption "Enable quickemu";
+    };
 
-  config = mkIf config.quickemu.enable {
-    home.packages = with pkgs; [
-      quickemu
-    ];
-  };
-}
+    config = mkIf config.quickemu.enable {
+      home.packages = with pkgs; [
+        quickemu
+      ];
+
+      programs.bash = {
+        enable = true;
+        shellAliases = {
+          windows11-setup = "bash ${windows11-setup}";
+          windows11 = "bash ${windows11}";
+        };
+      };
+    };
+  }
