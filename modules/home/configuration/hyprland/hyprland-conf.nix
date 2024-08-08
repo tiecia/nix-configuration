aa9369efb804a8a08114f3fdae1164149e8c6881
tiecia <ty.cia@outlook.com>
2024-08-07 21:43:37 -0700

    desktop 53 current  2024-08-07 21:43:36  24.11.20240804.cb9a96f  6.6.44                          plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -267,7 +267,7 @@ in
 
             general = {
               gaps_in = "2";
-              gaps_out = "1";
+              gaps_out = "10";
               border_size = "0";
               "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
               "col.inactive_border" = "rgba(595959aa)";
