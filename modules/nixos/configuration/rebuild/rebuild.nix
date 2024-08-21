ce3c32624e31565553f01e8b45d2bfae008234c2
tiecia <ty.cia@outlook.com>
2024-08-20 21:16:02 -0700

    sls 305 current  2024-08-20 21:15:59  24.11.20240811.a58bc8a  6.10.3                          gnome plasma

--- modules/nixos/configuration/rebuild/rebuild.nix
+++ modules/nixos/configuration/rebuild/rebuild.nix
@@ -26,6 +26,100 @@
 
     nvd diff /nix/var/nix/profiles/system-{$1,$2}-link
   '';
+
+  rebuild = pkgs.writeShellScriptBin "rebuild" ''
+    set -e
+
+
+    # cd to your config dir
+    pushd ~/nix-configuration
+
+    update=0
+    impure=0
+    dry=0
+    test=0
+    verbose=0
+    nopush=0
+
+    for arg in "$@"
+    do
+        if [ "$arg" == "-u" ] || [ "$arg" == "--update" ]; then
+            update=1
+        elif [ "$arg" == "-i" ] || [ "$arg" == "--impure" ]; then
+          impure=1
+        elif [ "$arg" == "-d" ] || [ "$arg" == "--dry" ]; then
+          dry=1
+        elif [ "$arg" == "-t" ] || [ "$arg" == "--test" ]; then
+          test=1
+        elif [ "$arg" == "-v" ] || [ "$arg" == "--verbose" ]; then
+          verbose=1
+        elif [ "$arg" == "-n" ] || [ "$arg" == "--nopush" ]; then
+      nopush=1
+        fi
+    done
+
+    # Autoformat your nix files
+    alejandra . &>/dev/null \ || ( alejandra . ; echo "formatting failed!" && exit 1)
+
+    # Shows your changes
+    git diff -U0 *.nix
+
+    sudo git add -A
+
+    echo "NixOS rebuilding with host configuration \"$CONFIGURATION_HOST\""
+
+    nix flake lock --update-input custom-nvim
+
+    options=""
+    # Rebuild, output simplified errors, log trackebacks
+    if [ $impure == 1 ]; then
+      sudo nixos-rebuild switch --impure --flake ./#$CONFIGURATION_HOST
+    else
+        if [ $dry == 1 ]; then
+            options+="--dry "
+        fi
+
+        if [ $verbose == 1 ]; then
+            options+="--verbose "
+        fi
+
+        if [ $update == 1 ]; then
+            options+="--update "
+        fi
+
+        if [[ ! -z $SPECIALISATION ]]; then
+      echo "Using specialisation \"$SPECIALISATION\""
+      options+="-s $SPECIALISATION "
+        fi
+
+        if [ $test == 1 ]; then
+            nh os test ./ -H $CONFIGURATION_HOST $options
+        else
+            nh os switch ./ -H $CONFIGURATION_HOST $options
+        fi
+    fi
+
+    # Get current generation metadata
+    current=$(nixos-rebuild list-generations --flake ./#$CONFIGURATION_HOST | grep current)
+
+    # Commit all changes witih the generation metadata
+    if [ $dry == 0 ]; then
+        sudo git commit -am "$CONFIGURATION_HOST $current"
+    fi
+
+    # if [ $nopush == 0 ]; then
+      # git push
+    # fi
+
+    sudo chown -R $USER ./
+
+    # Back to where you were
+    popd
+
+    echo "NixOS Rebuild OK!"
+  '';
+
+  rebuild-command = "bash ${rebuild}/bin/rebuild";
 in
   with lib; {
     options = {
@@ -49,15 +143,13 @@ in
         ];
 
         shellAliases = {
-          edit = "code ~/nix-configuration";
-          rebuild = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
-          rb = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
+          rebuild = rebuild-command;
+          rb = rebuild-command;
           listgens = "nix profile history --profile /nix/var/nix/profiles/system"; # https://nixos.org/manual/nix/stable/package-management/garbage-collection
           nixclean = "nh clean all --keep 10";
           cleannix = "nh clean all --keep 10";
           search = "nh search";
 
-          # compare = "bash ~/nix-configuration/modules/nixos/configuration/rebuild/compare.sh";
           compare = "bash ${compare}/bin/compare";
 
           ns = "nix-shell";
