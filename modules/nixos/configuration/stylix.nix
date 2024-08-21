dc3757bd6c10cd902bf6087897404ec95be87705
tiecia <ty.cia@outlook.com>
2024-08-20 21:29:16 -0700

    sls 307 current  2024-08-20 21:29:13  24.11.20240811.a58bc8a  6.10.3                          gnome plasma

--- modules/nixos/configuration/stylix.nix
+++ modules/nixos/configuration/stylix.nix
@@ -21,7 +21,7 @@ with lib; {
     image = lib.mkDefault ../../../wallpapers/alena-aenami-lights1k1.jpg;
     polarity = "dark";
 
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
 
     targets = {
       gnome.enable = lib.mkForce false;
