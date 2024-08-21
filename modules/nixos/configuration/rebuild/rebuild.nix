92b2c8db0a97585ce72cc0740abefb87ac8cc431
tiecia <ty.cia@outlook.com>
2024-08-20 21:10:23 -0700

    sls 304 current  2024-08-20 21:10:20  24.11.20240811.a58bc8a  6.10.3                          gnome plasma

--- modules/nixos/configuration/rebuild/rebuild.nix
+++ modules/nixos/configuration/rebuild/rebuild.nix
@@ -3,51 +3,75 @@
   lib,
   pkgs,
   ...
-}:
-with lib; {
-  options = {
-    rebuild.enable = mkEnableOption "Enable the nixos-rebuild script";
-    rebuild.host = mkOption {
-      type = types.str;
-      default = "default";
-      description = "The name of the configuration host";
-    };
-  };
+}: let
+  compare = pkgs.writeShellScriptBin "compare" ''
+    # No arguments compares the most recent two generations.
+    # Two arguments compare the specified generations.
 
-  config = let
-    inherit (config.rebuild) host;
-  in {
-    environment = {
-      systemPackages = with pkgs; [
-        alejandra # .nix formatter
-        libnotify # Provides the notify-send used in my nixos-rebuild script
-        nh
-        nvd
-      ];
+    if [ $# == 0 ]; then
+        current=$(readlink /nix/var/nix/profiles/system | grep -o "[0-9]*")
+        prev=$(($current-1))
+        nvd diff /nix/var/nix/profiles/system-{$prev,$current}-link
+        exit 0
+    fi
 
-      shellAliases = {
-        edit = "code ~/nix-configuration";
-        rebuild = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
-        rb = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
-        listgens = "nix profile history --profile /nix/var/nix/profiles/system"; # https://nixos.org/manual/nix/stable/package-management/garbage-collection
-        nixclean = "nh clean all --keep 10";
-        cleannix = "nh clean all --keep 10";
-        search = "nh search";
 
-        compare = "bash ~/nix-configuration/modules/nixos/configuration/rebuild/compare.sh";
+    if [ $# -lt 2 ]; then
+      echo 1>&2 "$0: not enough arguments"
+      exit 2
+    elif [ $# -gt 2 ]; then
+      echo 1>&2 "$0: too many arguments"
+      exit 2
+    fi
 
-        ns = "nix-shell";
-        try = "nix-shell -p";
-
-        conf = "nvim ~/nix-configuration/hosts/${host}/configuration.nix";
-        home = "nvim ~/nix-configuration/hosts/${host}/home.nix";
-        nxrs = "sudo nixos-rebuild switch --flake ~/nix-configuration/#${host}";
-        nxrt = "sudo nixos-rebuild test --flake ~/nix-configuration/#${host}";
-      };
-
-      sessionVariables = {
-        CONFIGURATION_HOST = host;
+    nvd diff /nix/var/nix/profiles/system-{$1,$2}-link
+  '';
+in
+  with lib; {
+    options = {
+      rebuild.enable = mkEnableOption "Enable the nixos-rebuild script";
+      rebuild.host = mkOption {
+        type = types.str;
+        default = "default";
+        description = "The name of the configuration host";
       };
     };
-  };
-}
+
+    config = let
+      inherit (config.rebuild) host;
+    in {
+      environment = {
+        systemPackages = with pkgs; [
+          alejandra # .nix formatter
+          libnotify # Provides the notify-send used in my nixos-rebuild script
+          nh
+          nvd
+        ];
+
+        shellAliases = {
+          edit = "code ~/nix-configuration";
+          rebuild = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
+          rb = "~/nix-configuration/modules/nixos/configuration/rebuild/rebuild.sh";
+          listgens = "nix profile history --profile /nix/var/nix/profiles/system"; # https://nixos.org/manual/nix/stable/package-management/garbage-collection
+          nixclean = "nh clean all --keep 10";
+          cleannix = "nh clean all --keep 10";
+          search = "nh search";
+
+          # compare = "bash ~/nix-configuration/modules/nixos/configuration/rebuild/compare.sh";
+          compare = "bash ${compare}/bin/compare";
+
+          ns = "nix-shell";
+          try = "nix-shell -p";
+
+          conf = "nvim ~/nix-configuration/hosts/${host}/configuration.nix";
+          home = "nvim ~/nix-configuration/hosts/${host}/home.nix";
+          nxrs = "sudo nixos-rebuild switch --flake ~/nix-configuration/#${host}";
+          nxrt = "sudo nixos-rebuild test --flake ~/nix-configuration/#${host}";
+        };
+
+        sessionVariables = {
+          CONFIGURATION_HOST = host;
+        };
+      };
+    };
+  }
