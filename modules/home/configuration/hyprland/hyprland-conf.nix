de53081c0b810c60b83745c2456079f3d9e0766f
tiecia <ty.cia@outlook.com>
2024-08-25 14:36:26 -0700

    desktop 115 current  2024-08-25 14:36:25  24.11.20240818.8a33541  6.6.46                          gnome plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -79,6 +79,7 @@ in
         wayland.windowManager.hyprland = {
           enable = true;
           package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+          systemd.variables = ["--all"];
           settings = {
             inherit (options) monitor;
 
