fd469e13d26e5bcf9d426bb81b034cab16af3700
tiecia <ty.cia@outlook.com>
2024-08-26 12:22:19 -0700

    desktop 140 current  2024-08-26 12:22:18  24.11.20240824.d0e1602  6.6.47                          gnome plasma

--- flake.nix
+++ flake.nix
@@ -19,11 +19,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # NOTE: hyprland and hyprsplit must be updated together
+    # NOTE: hyprland and hyprsplit should be updated together
     # A couple of commits after v1.41.2
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=9a09eac79b85c846e3a865a9078a3f8ff65a9259";
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1&rev=09dbcabcc71f412dcaf86dbed7b67d58a273fa50";
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     # v1.41.2
     hyprsplit = {
