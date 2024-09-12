283e6cc6d02b8199740ec058635c8887666d53a6
tiecia <ty.cia@outlook.com>
2024-09-11 21:08:58 -0700

    desktop 159 current  2024-09-11 21:08:57  24.11.20240906.574d1ea  6.6.49                          gnome plasma

--- modules/home/configuration/hyprland/hyprland-conf.nix
+++ modules/home/configuration/hyprland/hyprland-conf.nix
@@ -588,7 +588,8 @@ in
               };
               listener = {
                 timeout = 600; # in seconds (10 minutes)
-                on-timeout = "systemctl suspend"; # command to run when timeout has passed
+                on-timeout = "hyprlock"; # command to run when timeout has passed
+                # on-timeout = "systemctl suspend"; # command to run when timeout has passed
                 # on-resume = "notify-send 'Welcome back!'"; # command to run when activity is detected after timeout has fired.
               };
 
