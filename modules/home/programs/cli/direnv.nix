12efae0c2c6f043cd72625e65f057a9e6f72e7a7
tiecia <ty.cia@outlook.com>
2024-09-27 20:12:27 -0700

    desktop 169 current  2024-09-27 20:12:26  24.11.20240921.9357f4f  6.6.52                          gnome plasma

--- modules/home/programs/cli/direnv.nix
+++ modules/home/programs/cli/direnv.nix
@@ -23,6 +23,24 @@ with lib; {
         bashrcExtra = ''
           eval "$(direnv hook bash)"
 
+          nixify() {
+            if [ ! -e ./.envrc ]; then
+              echo "use nix" > .envrc
+              direnv allow
+            fi
+            if [[ ! -e shell.nix ]] && [[ ! -e default.nix ]]; then
+              cat > default.nix <<'EOF'
+          with import <nixpkgs> {};
+          mkShell {
+            nativeBuildInputs = [
+              bashInteractive
+            ];
+          }
+          EOF
+              ''${EDITOR:-vim} default.nix
+            fi
+          }
+
           flakify() {
             if [ ! -e flake.nix ]; then
               nix flake new -t github:nix-community/nix-direnv .
