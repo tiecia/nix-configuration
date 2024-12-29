b1d48c460a3842a9baa38e7c0fbcd1fe69d28f90
tiecia <ty.cia@outlook.com>
2024-12-28 22:52:23 -0800

    sls 29 current  2024-12-28 22:52:22  25.05.20241211.5d67ea6     6.10.5                          hyprland plasma

--- modules/nixos/programs/desktop/wireshark.nix
+++ modules/nixos/programs/desktop/wireshark.nix
@@ -10,6 +10,10 @@ with lib; {
   };
 
   config = mkIf config.wireshark.enable {
+    environment.systemPackages = [
+      pkgs.wireshark
+    ];
+
     # Installs the program and registers the proper user groups.
     # https://search.nixos.org/options?channel=unstable&show=programs.wireshark.enable&from=0&size=50&sort=relevance&type=packages&query=wireshark
     programs.wireshark.enable = true;
