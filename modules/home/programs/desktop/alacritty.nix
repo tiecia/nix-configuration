f6ed55638215134451825b1f5904f4e321b0d814
tiecia <ty.cia@outlook.com>
2025-01-02 22:08:37 -0800

    sls 43 current  2025-01-02 22:08:34  25.05.20241211.5d67ea6     6.10.5                          hyprland plasma

--- modules/home/programs/desktop/alacritty.nix
+++ modules/home/programs/desktop/alacritty.nix
@@ -2,7 +2,6 @@
   config,
   lib,
   pkgs,
-  stdenv,
   ...
 }: let
   alacritty-theme = pkgs.callPackage ./alacritty-theme.nix {};
@@ -13,14 +12,26 @@ in
     };
 
     config = mkIf config.alacritty.enable {
-      home.packages = with pkgs; [
-        alacritty
-      ];
+      # home.packages = with pkgs; [
+      #   alacritty
+      # ];
+
+      programs.alacritty = {
+        enable = true;
+        settings = lib.mkForce {
+          general.import = [
+            "alacritty-theme/themes/gruvbox_dark.toml"
+          ];
+          window = {
+            startup_mode = "Maximized";
+            opacity = 0.9;
+          };
+          font.size = 14;
+        };
+      };
 
       home.file = {
         ".config/alacritty/alacritty-theme".source = alacritty-theme;
-        #   source = alacritty-theme;
-        # };
       };
     };
   }
