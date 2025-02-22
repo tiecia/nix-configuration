1448efd55c28d30fe15e8f01eca2d233c641f2ec
tiecia <ty.cia@outlook.com>
2025-02-22 14:04:21 -0800

    desktop 223 current  2025-02-22 14:04:20  25.05.20250213.2ff53fe  6.12.13                          hyprland plasma

--- modules/nixos/programs/desktop/virtualbox.nix
+++ modules/nixos/programs/desktop/virtualbox.nix
@@ -11,12 +11,17 @@ with lib; {
 
   config = mkIf config.virtualbox.enable {
     virtualisation.virtualbox = {
-      host.enable = true;
+      host = {
+        enable = true;
+      };
       guest = {
         enable = true;
         dragAndDrop = true;
       };
     };
+
+    # https://github.com/NixOS/nixpkgs/issues/363887#issuecomment-2536693220
+    boot.kernelParams = ["kvm.enable_virt_at_load=0"];
 
     users.extraGroups.vboxusers.members = ["tiec"];
   };
