17b94ee340e4c4958a874f534f752d071332b91d
tiecia <ty.cia@outlook.com>
2025-03-03 11:06:07 -0800

    sls 58 current  2025-03-03 11:06:06  25.05.20250213.2ff53fe  6.12.7                          hyprland plasma

--- modules/nixos/configuration/rebuild.nix
+++ modules/nixos/configuration/rebuild.nix
@@ -146,8 +146,8 @@ in
           rebuild = rebuild-command;
           rb = rebuild-command;
           listgens = "nix profile history --profile /nix/var/nix/profiles/system"; # https://nixos.org/manual/nix/stable/package-management/garbage-collection
-          nixclean = "nh clean all --keep 3";
-          cleannix = "nh clean all --keep 3";
+          nixclean = "nh clean all --keep 3 --nogcroots";
+          cleannix = "nh clean all --keep 3 --nogcroots";
           search = "nh search";
 
           compare = "bash ${compare}/bin/compare";
