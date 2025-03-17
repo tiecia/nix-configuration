484a46f3b8d5264e347febd75a60d16f3b92f182
tiecia <ty.cia@outlook.com>
2025-03-17 11:14:58 -0700

    add command

--- services/test/src/main.rs
+++ services/test/src/main.rs
@@ -1,3 +1,8 @@
+use std::process::Command;
+
 fn main() {
     println!("Hello, world!");
+
+    Command::new("pwd").status().expect("Unable to run command");
+    Command::new("ls").status().expect("Unable to run command");
 }
