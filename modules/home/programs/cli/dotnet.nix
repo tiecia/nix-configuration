75dee91170426641833ad52cd147869639e8a4e1
tiecia <ty.cia@outlook.com>
2025-03-19 19:49:36 -0700

    desktop 278 current  2025-03-19 19:49:34  25.05.20250315.c80f6a7  6.12.19                          hyprland plasma

--- modules/home/programs/cli/dotnet.nix
+++ modules/home/programs/cli/dotnet.nix
@@ -14,6 +14,31 @@ in {
 
   config = let
     netcoredbg = pkgs.netcoredbg;
+
+    dotnet-root = "share/dotnet";
+    # This is needed to install workload in $HOME
+    # https://discourse.nixos.org/t/dotnet-maui-workload/20370/2
+    userlocal = ''
+       for i in $out/${dotnet-root}/sdk/*; do
+         i=$(basename $i)
+         length=$(printf "%s" "$i" | wc -c)
+         substring=$(printf "%s" "$i" | cut -c 1-$(expr $length - 2))
+         i="$substring""00"
+         mkdir -p $out/${dotnet-root}/metadata/workloads/''${i/-*}
+         touch $out/${dotnet-root}/metadata/workloads/''${i/-*}/userlocal
+      done
+    '';
+    # append userlocal sctipt to postInstall phase
+    postInstallUserlocal = finalAttrs: previousAttrs: {
+      postInstall = (previousAttrs.postInstall or '''') + userlocal;
+    };
+    # append userlocal sctipt to postBuild phase
+    postBuildUserlocal = finalAttrs: previousAttrs: {
+      postBuild = (previousAttrs.postBuild or '''') + userlocal;
+    };
+
+    # use this if you don't need multiple SDK versions
+    dotnet-combined = pkgs.dotnetCorePackages.sdk_9_0.unwrapped.overrideAttrs postInstallUserlocal;
   in
     lib.mkIf config.dotnet.enable
     {
@@ -24,10 +49,12 @@ in {
         # pkgs.dotnetCorePackages.sdk_8_0_1xx
         # pkgs.dotnetCorePackages.sdk_9_0_1xx
 
-        (pkgs.dotnetCorePackages.combinePackages [
-          pkgs.dotnetCorePackages.sdk_9_0
-          pkgs.dotnetCorePackages.sdk_8_0
-        ])
+        # (pkgs.dotnetCorePackages.combinePackages [
+        #   pkgs.dotnetCorePackages.sdk_9_0
+        #   pkgs.dotnetCorePackages.sdk_8_0
+        # ])
+
+        dotnet-combined
 
         pkgs.azure-cli
       ];
