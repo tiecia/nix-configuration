55a4d35215feeea9e605a96d715a0dc9db6e9341
tiecia <ty.cia@outlook.com>
2025-03-27 20:01:42 -0700

    desktop 294 current  2025-03-27 20:01:42  25.05.20250315.c80f6a7  6.12.19                          hyprland plasma

--- modules/home/programs/desktop/microsoft-edge.nix
+++ modules/home/programs/desktop/microsoft-edge.nix
@@ -9,9 +9,23 @@ with lib; {
     microsoft-edge.enable = lib.mkEnableOption "Enable microsoft-edge";
   };
 
-  config = mkIf config.microsoft-edge.enable {
-    home.packages = with pkgs; [
-      microsoft-edge
-    ];
-  };
+  config = let
+    microsoft-edge = pkgs.microsoft-edge;
+  in
+    mkIf config.microsoft-edge.enable {
+      home.packages = [
+        microsoft-edge
+      ];
+
+      xdg.desktopEntries = {
+        microsoft-edge = {
+          name = "Microsoft Edge";
+          genericName = "Web Browser";
+          exec = "${microsoft-edge}/bin/microsoft-edge";
+          icon = "microsoft-edge";
+          categories = ["Network" "WebBrowser"];
+          startupNotify = true;
+        };
+      };
+    };
 }
