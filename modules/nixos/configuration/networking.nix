c425030d25d20b407b3f6ee2636f1b48dabc74f0
tiecia <ty.cia@outlook.com>
2025-03-29 16:35:15 -0700

    desktop 297 current  2025-03-29 16:35:15  25.05.20250315.c80f6a7  6.12.19                          hyprland plasma

--- modules/nixos/configuration/networking.nix
+++ modules/nixos/configuration/networking.nix
@@ -12,5 +12,10 @@
     networkmanager.enable = lib.mkDefault true;
     wireless.enable = lib.mkForce false;
     hostName = lib.mkDefault "nixos";
+
+    extraHosts = ''
+      34.169.118.232 install
+      104.198.96.36 runner
+    '';
   };
 }
