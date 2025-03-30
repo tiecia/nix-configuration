32aabd783feaaabae9fc3cc91110efcc8690f43a
tiecia <ty.cia@outlook.com>
2025-03-29 20:55:06 -0700

    sls 97 current  2025-03-29 20:54:49  25.05.20250315.c80f6a7  6.12.17                          hyprland plasma (HM: 2025-03-29 20:50 : id 61)

--- modules/nixos/configuration/stylix.nix
+++ modules/nixos/configuration/stylix.nix
@@ -5,25 +5,19 @@
   ...
 }:
 with lib; {
-  stylix = {
-    enable = lib.mkDefault true;
-    image = lib.mkDefault ../../../wallpapers/dark-water.jpg;
-    polarity = "dark";
-
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
-
-    autoEnable = false;
-
-    targets = {
-      # gnome.enable = lib.mkForce false;
-      # spicetify.enable = lib.mkForce false;
-      # gtk.enable = lib.mkForce true;
-    };
-
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Classic";
-      size = 12;
-    };
-  };
+  # stylix = {
+  #   enable = lib.mkDefault true;
+  #   # image = lib.mkDefault ../../../wallpapers/dark-water.jpg;
+  #   polarity = "dark";
+  #
+  #   base16Scheme = "${pkgs.base16-schemes}/share/themes/ayu-mirage.yaml";
+  #
+  #   autoEnable = false;
+  #
+  #   cursor = {
+  #     package = pkgs.bibata-cursors;
+  #     name = "Bibata-Modern-Classic";
+  #     size = 12;
+  #   };
+  # };
 }
