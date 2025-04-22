3ef66c5ba6e074dbd466207c2d5618ce5a12b37b
tiecia <ty.cia@outlook.com>
2025-04-22 15:44:17 -0700

    sls 108 current  2025-04-14 14:56:23  25.05.20250412.2631b0b  6.12.17                          hyprland plasma (HM: 2025-04-22 15:44 : id 69)

--- modules/home/programs/cli/default.nix
+++ modules/home/programs/cli/default.nix
@@ -28,6 +28,7 @@
     ./bash.nix
     ./poppler.nix
     ./dig.nix
+    ./traceroute.nix
   ];
 
   # Programs that are enabled by default when the home/programs/cli directory is imported
@@ -52,6 +53,7 @@
   bash.enable = lib.mkDefault true;
   poppler-pdf-tools.enable = lib.mkDefault true;
   dig.enable = lib.mkDefault true;
+  traceroute.enable = lib.mkDefault true;
 
   dotnet.enable = lib.mkDefault true;
 }
